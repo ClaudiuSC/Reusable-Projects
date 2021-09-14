28a4d1c356863e16c50e6a8473b008018dd28689
ClaudiuSC <67117889+ClaudiuSC@users.noreply.github.com>
2021-09-14 21:32:47 +0300

    Add files via upload

---
+++ Progress-steps/script.js
@@ -0,0 +1,49 @@
+const progressEl = document.getElementById("progress")
+const prevBtn = document.getElementById("prev")
+const nextBtn = document.getElementById("next")
+const circlesEl = document.querySelectorAll(".circle")
+
+let currentActive = 1
+
+nextBtn.addEventListener("click", () => {
+     currentActive++
+
+     if(currentActive > circlesEl.length) {
+         currentActive = circlesEl.length
+     }
+
+    update()
+})
+
+prevBtn.addEventListener("click", () => {
+    currentActive--
+    if(currentActive < 1 ) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+function update() {
+    circlesEl.forEach((circle, index) => {
+        if(index < currentActive) {
+            circle.classList.add("active")
+        }
+        else {
+            circle.classList.remove("active")
+        }
+    })
+
+    const actives = document.querySelectorAll(".active")
+
+    progress.style.width = (actives.length-1) / (circlesEl.length-1) *100 + "%"
+
+    if(currentActive === 1) {
+        prevBtn.disabled = true
+    } else if(currentActive === circlesEl.length) {
+        nextBtn.disabled = true
+    } else {
+        prevBtn.disabled = false
+        nextBtn.disabled = false
+    }
+}
