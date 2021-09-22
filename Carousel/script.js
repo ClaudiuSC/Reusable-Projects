d70ac4e79f2fb63ff336f4d768f4602f728bbd40
ClaudiuSC <67117889+ClaudiuSC@users.noreply.github.com>
2021-09-22 23:42:26 +0300

    Update script.js

--- Carousel/script.js
+++ Carousel/script.js
@@ -3,18 +3,6 @@ const nextBtn = document.querySelector(".carousel-btn-next")
 const previousBtn = document.querySelector(".carousel-btn-previous")
 let counter = 0
 
-
-// test for progress steps
-// const progressEl = document.querySelector(".progress-container")
-// for (let i = 0; i < carouselItems.length; i++) {
-//     progressEl.innerHTML += `<div><span class="circle">${i+1}</span></div>`
-// }
-// const circleEl = document.querySelectorAll(".circle")
-// -------------------------------------------------------------
-
-
-// circleProgress()
-
 nextBtn.addEventListener("click", ()=> {
     removeClass(carouselItems)
     counter++
@@ -41,16 +29,5 @@ function removeClass(item) {
         i.classList.remove("visible", "animation")
     }
 }
-
-
-// test for progress steps
-
-// function circleProgress() {
-//     for(let i = 0; i < circleEl.length; i++) {
-//         circleEl[i].classList.remove("circle-active")
-//     }
-
-//     circleEl[counter].classList.add("circle-active")
-// }
 
 
