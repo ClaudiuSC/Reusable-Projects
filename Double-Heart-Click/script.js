88775d47a9f0cf4aa8241331b4963dcc4233ed38
Claudiu Stoican <67117889+ClaudiuSC@users.noreply.github.com>
2022-01-24 16:20:13 +0200

    Add files via upload

---
+++ Double-Heart-Click/script.js
@@ -0,0 +1,40 @@
+const loveMe = document.querySelector(".loveMe")
+const times = document.querySelector("#times")
+let i = 1
+
+loveMe.addEventListener("dblclick", (e) => {
+    createHeart(e)
+    times.textContent = i++
+})
+
+function createHeart(e) {
+    const x = e.clientX - e.target.offsetLeft
+    const y = e.clientY - e.target.offsetTop
+
+    const heart = document.createElement("i")
+    heart.style.top = y + "px"
+    heart.style.left = x + "px"
+    heart.className = "fas fa-heart"
+
+    loveMe.append(heart)
+    setTimeout(() => heart.remove(), 600)
+}
+
+// =====================================================
+// DOUBLE CLICK FUNCTIONALITY
+// =====================================================
+
+// let clickTime = 0
+
+// loveMe.addEventListener("click", (e) => {
+//     if(clickTime === 0) {
+//         clickTime = new Date().getTime()
+//     } else {
+//         if (new Date().getTime() - clickTime < 500) {
+//             createHeart(e)
+//             times.textContent = i++
+//         } else {
+//             clickTime = new Date().getTime()
+//         }
+//     }
+// })
